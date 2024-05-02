67d517b36803df2a83f897dbace267d0724e730e
Laminesaidi <166522750+Laminesaidi@users.noreply.github.com>
2024-05-02 13:34:53 +0200

    comm

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ fetch("instrumentsMusiques.json")
     addinstrument(donnees.entreprise.produits);
     addservice (donnees.entreprise.services);
     addtemoignages(donnees.entreprise.temoignages);
-
+   
   });
 
   
@@ -72,6 +72,21 @@ fetch("instrumentsMusiques.json")
          `
         }) 
       }
+      
+      var map = L.map('map').setView([45.4314190972094, 4.40174009945124], 13);
+
+var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+})
+.addTo(map);
+
+L.marker([45.4314190972094, 4.40174009945124]).addTo(map)
+    .bindPopup("MusicHalle - Instruments du Monde, 42009 Saint-Étienne")
+    .openPopup();
+    
 
       
     
